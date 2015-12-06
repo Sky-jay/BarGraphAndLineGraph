956c65238c213cfee1b26ebccc814e9de5ae6e60
Sky-jay <lizeshaogaofeng@163.com>
2015-12-06 22:29:36 +0800

    Add Some Bar Graph With Gradient.

--- BarGraphAndLineGraph/BarGraphAndLineGraph/Views/BarView.m
+++ BarGraphAndLineGraph/BarGraphAndLineGraph/Views/BarView.m
@@ -52,6 +52,39 @@
     }
     CGContextRestoreGState(ctx);
     
+    [self drawBarGraphWithHeight:170 location:1  rect:rect andRGBColorR:1 G:0 B:0];
+    [self drawBarGraphWithHeight:270 location:2  rect:rect andRGBColorR:0 G:1 B:0];
+    [self drawBarGraphWithHeight:370 location:3  rect:rect andRGBColorR:0 G:0 B:1];    
+}
+
+- (void)drawBarGraphWithHeight:(CGFloat)height location:(int)index rect:(CGRect)rect andRGBColorR:(CGFloat)r G:(CGFloat)g B:(CGFloat)b
+{
+    CGFloat BarX = 10 + 100 * index;
+    CGFloat BarStartY = kRectH - 10;
+    CGFloat BarEndY = kRectH - 10 - height;
+    CGFloat BarW = 70;
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    CGContextSaveGState(ctx);
+    CGContextMoveToPoint(ctx, BarX, BarStartY);
+    CGContextAddLineToPoint(ctx, BarX, BarEndY);
+    CGContextSetRGBStrokeColor(ctx, 1, 0, 0, 1);
+    CGContextSetLineWidth(ctx, BarW);
+    //    CGContextStrokePath(ctx);
+    CGContextReplacePathWithStrokedPath(ctx);
+    CGContextClip(ctx);
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    const CGFloat components[] = {
+        r, g, b, 1,
+        1, 1, 1, 1,
+        r, g, b, 1
+    };
+    const CGFloat locations[] ={0, 0.3, 1};
+    CGGradientRef gradient = CGGradientCreateWithColorComponents(colorSpace, components, locations, 3);
+    CGContextDrawLinearGradient(ctx, gradient, CGPointMake(BarX - BarW/2, BarStartY), CGPointMake(BarX + BarW/2, BarStartY), 0);
+    CGColorSpaceRelease(colorSpace);
+    CGGradientRelease(gradient);
+    CGContextRestoreGState(ctx);
 }
 
 @end
